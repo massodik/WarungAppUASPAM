a1f5d42ae58ba9269d200c957f06171980077972
aqsal <muhammadaqsal96@gmail.com>
2022-12-28 12:18:24 +0700

    first commit

---
+++ app/src/main/java/com/aqsal/pizza/InterfaceAPI.java
@@ -0,0 +1,11 @@
+package com.aqsal.pizza;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface InterfaceAPI {
+
+    @GET("uasresto")
+    Call<List<Posts>> getPosts();
+}
